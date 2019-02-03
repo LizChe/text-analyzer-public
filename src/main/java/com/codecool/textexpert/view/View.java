708dc1caf2907e83571fcebc16b842ae9618a316
Liz <pachaffy@gmail.com>
2019-02-03 14:41:38 +0100

    Implement method that prints the logo

--- src/main/java/com/codecool/textexpert/view/View.java
+++ src/main/java/com/codecool/textexpert/view/View.java
@@ -1,4 +1,31 @@
 package com.codecool.textexpert.view;
 
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+
 public class View {
+
+    public void printMessage(String message) {
+        System.out.println(message);
+    }
+
+    public void printLogo() {
+        Scanner logoFile = null;
+        StringBuilder logo = new StringBuilder();
+
+        try {
+            logoFile = new Scanner(new File("src/main/resources/ui/logo.txt"));
+        } catch (FileNotFoundException e) {
+            printMessage(e.getMessage());
+        }
+
+        while (logoFile.hasNext()) {
+            logo.append(logoFile.nextLine()).append("\n");
+        }
+
+        printMessage(logo.toString());
+    }
 }
