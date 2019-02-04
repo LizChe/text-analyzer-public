dda601954f2b5ea78aa9ddb86584374d03640f01
Liz <pachaffy@gmail.com>
2019-02-04 23:55:14 +0100

    Implement CharIterator

--- src/main/java/com/codecool/textexpert/iterator/CharIterator.java
+++ src/main/java/com/codecool/textexpert/iterator/CharIterator.java
@@ -1,32 +1,47 @@
-package com.codecool.textexpert.iterators;
+package com.codecool.textexpert.iterator;
 
-import com.codecool.textexpert.dao.TextFileDao;
-
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
-public class CharIterator implements Iterator {
+import com.codecool.textexpert.dao.FileContent;
 
-    private TextFileDao textFileDao;
+public class CharIterator implements Iterator<String> {
 
-    public CharIterator(TextFileDao textFileDao) {
-        textFileDao = new TextFileDao(textFileDao.getFileName());
+    private FileContent fileContent;
+    private List<String> chars;
+    private int index;
+
+    public CharIterator(FileContent fileContent) {
+        this.fileContent = fileContent;
     }
 
+    @Override
     public boolean hasNext() {
-        /**
-         * Implement the method here
-         */
+        return index < chars.size();
     }
 
+    @Override
     public String next() {
-        /**
-         * Implement the method here
-         */
+        if (this.hasNext()) {
+            return chars.get(index++);
+        }
+        return null;
     }
 
-    public void remove() {
-        /**
-         * Implement the method here
-         */
+    private void addChars(FileContent fileContent) {
+        List<String> fileContentToParse = fileContent.getFileContent();
+        chars = new ArrayList<>();
+
+        for (char letter : fileContentToParse.toString().toCharArray()) {
+            if (Character.isLetter(letter)) {
+                chars.add(Character.toString(letter));
+            }
+        }
+    }
+
+    public List<String> getChars() {
+        addChars(fileContent);
+        return chars;
     }
 }
