95ac518c604c5d0e3e9969e5930ed940f9a9788e
Liz <pachaffy@gmail.com>
2019-02-05 00:24:01 +0100

    Implement WordIterator

--- src/main/java/com/codecool/textexpert/iterator/WordIterator.java
+++ src/main/java/com/codecool/textexpert/iterator/WordIterator.java
@@ -1,32 +1,46 @@
-package com.codecool.textexpert.iterators;
+package com.codecool.textexpert.iterator;
 
-import com.codecool.textexpert.dao.TextFileDao;
-
+import java.util.Arrays;
 import java.util.Iterator;
+import java.util.List;
 
-public class WordIterator implements Iterator {
+import com.codecool.textexpert.dao.FileContent;
 
-    private TextFileDao textFileDao;
+public class WordIterator implements Iterator<String> {
 
-    public WordIterator(TextFileDao textFileDao) {
-        textFileDao = new TextFileDao(textFileDao.getFileName());
+    private FileContent fileContent;
+    private List<String> words;
+    private int index;
+
+    public WordIterator(FileContent fileContent) {
+        this.fileContent = fileContent;
     }
 
+    @Override
     public boolean hasNext() {
-        /**
-         * Implement the method here
-         */
+        return index < words.size();
     }
 
+    @Override
     public String next() {
-        /**
-         * Implement the method here
-         */
+        if (this.hasNext()) {
+            return words.get(index++);
+        }
+        return null;
     }
 
-    public void remove() {
-        /**
-         * Implement the method here
-         */
+    private void addWords(FileContent fileContent) {
+        List<String> fileContentToParse = fileContent.getFileContent();
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (String string : fileContentToParse) {
+            stringBuilder.append(string);
+        }
+        words = Arrays.asList(stringBuilder.toString().split("\\s+"));
+    }
+
+    public List<String> getWords() {
+        addWords(fileContent);
+        return words;
     }
 }
