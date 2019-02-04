f3791f8f4fb3acfa5d8e03972ddf11c8609b3404
Liz <pachaffy@gmail.com>
2019-02-04 21:28:57 +0100

    Add loadFileContent method and a getter for the list of strings

--- src/main/java/com/codecool/textexpert/models/FileContent.java
+++ src/main/java/com/codecool/textexpert/models/FileContent.java
@@ -1,14 +1,21 @@
 package com.codecool.textexpert.models;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
 import com.codecool.textexpert.iterators.CharIterator;
 import com.codecool.textexpert.iterators.TextIterator;
 import com.codecool.textexpert.iterators.WordIterator;
 
-import java.util.Iterator;
-
 public class FileContent implements TextIterator {
 
     private String fileName;
+    private List<String> fileContent;
 
     public FileContent(String fileName) {
         this.fileName = fileName;
@@ -24,5 +31,26 @@ public class FileContent implements TextIterator {
 
     public String getFileName() {
         return fileName;
+    }
+
+    private void loadFileContent(String fileName) {
+        fileContent = new ArrayList<>();
+        Scanner textFile = null;
+
+        try {
+            textFile = new Scanner(new File(fileName));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        while (textFile.hasNext()) {
+            fileContent.add(textFile.nextLine());
+        }
+        textFile.close();
+    }
+
+    public List<String> getFileContent() {
+        loadFileContent(fileName);
+        return fileContent;
     }
 }
