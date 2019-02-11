e87cd2657fd4c17685439d2873cf2a4f2670ea06
Liz <pachaffy@gmail.com>
2019-02-11 19:17:22 +0100

    Implement Main

--- src/main/java/com/codecool/textexpert/main/Main.java
+++ src/main/java/com/codecool/textexpert/main/Main.java
@@ -1,1 +1,10 @@
+package com.codecool.textexpert.main;
 
+import com.codecool.textexpert.controller.StatisticalAnalysisController;
+
+public class Main {
+    public static void main(String[] args) {
+        StatisticalAnalysisController controller = new StatisticalAnalysisController(args);
+        controller.run();
+    }
+}
