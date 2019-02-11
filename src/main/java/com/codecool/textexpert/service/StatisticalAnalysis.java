ae3d8230527071dd19f923d53f30a07867f4e4e5
Liz <pachaffy@gmail.com>
2019-02-11 12:30:00 +0100

    Implement occurMoreThan method

--- src/main/java/com/codecool/textexpert/service/StatisticalAnalysis.java
+++ src/main/java/com/codecool/textexpert/service/StatisticalAnalysis.java
@@ -14,17 +14,17 @@ public class StatisticalAnalysis {
     public StatisticalAnalysis(Iterator<String> iterator) {
         this.iterator = iterator;
     }
- 
+
     public int countOf(String... elems) {
         int counter = 0;
         while (iterator.hasNext()) {
-            if (Arrays.stream(elems).anyMatch(iterator.next() :: equals)) {
-                counter ++;
+            if (Arrays.stream(elems).anyMatch(iterator.next()::equals)) {
+                counter++;
             }
         }
         return counter;
     }
- 
+
     public int dictionarySize() {
         Set<String> uniqueWords = new HashSet<>();
         while (iterator.hasNext()) {
@@ -32,19 +32,34 @@ public class StatisticalAnalysis {
         }
         return uniqueWords.size();
     }
- 
+
     public int size() {
         int counter = 0;
         while (iterator.hasNext()) {
             iterator.next();
-            counter ++;
+            counter++;
         }
         return counter;
     }
- 
+
     public Set<String> occurMoreThan(Integer n) {
-        /**
-         * @return all the elements that occur more than n_times in the dataset
-         */
+        Map<String, Integer> countOfUniqueWords = new HashMap<>();
+        while (iterator.hasNext()) {
+            String word = iterator.next();
+            if (countOfUniqueWords.containsKey(word)) {
+                countOfUniqueWords.put(word, countOfUniqueWords.get(word) + 1);
+            } else {
+                countOfUniqueWords.put(word, 1);
+            }
+        }
+
+        Set<String> occuredMoreThanN = new HashSet<>();
+        while (iterator.hasNext()) {
+            String word = iterator.next();
+            if (countOfUniqueWords.get(word) > n && !countOfUniqueWords.containsKey(word)) {
+                occuredMoreThanN.add(word);
+            }
+        }
+        return occuredMoreThanN;
     }
 }
