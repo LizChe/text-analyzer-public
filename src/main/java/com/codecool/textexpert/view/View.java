cd520d143468bfe41ca9153c64de90a227174d14
Liz <pachaffy@gmail.com>
2019-02-12 12:21:16 +0100

    Add method that prints formatted message

--- src/main/java/com/codecool/textexpert/view/View.java
+++ src/main/java/com/codecool/textexpert/view/View.java
@@ -1,15 +1,17 @@
 package com.codecool.textexpert.view;
 
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
-
 
 public class View {
 
     public void printMessage(String message) {
         System.out.println(message);
+    }
+
+    public void printFormattedMessage(String format, Object... args) {
+        System.out.printf(format, args);
     }
 
     public void printLogo() {
@@ -25,7 +27,6 @@ public class View {
         while (logoFile.hasNext()) {
             logo.append(logoFile.nextLine()).append("\n");
         }
-
         printMessage(logo.toString());
     }
 }
