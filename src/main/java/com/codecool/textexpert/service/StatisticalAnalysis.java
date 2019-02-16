416ca6910aa1ec365d83b74d98e90e1192e09775
Liz <pachaffy@gmail.com>
2019-02-16 11:37:30 +0100

    Move private methods of Service Controller to service module

--- src/main/java/com/codecool/textexpert/service/StatisticalAnalysis.java
+++ src/main/java/com/codecool/textexpert/service/StatisticalAnalysis.java
@@ -7,6 +7,8 @@ import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 
+import com.codecool.textexpert.view.View;
+
 public class StatisticalAnalysis {
 
     private Iterator<String> iterator;
@@ -62,7 +64,43 @@ public class StatisticalAnalysis {
         return occurredMoreThanN;
     }
 
-    public Set<String> getUniqueChars() {
+    public int calculateOnePercentOfWords() {
+        Double onePercentOfWords = 0.01 * size();
+        return onePercentOfWords.intValue();
+    }
+
+    public String countVowelsPercentage() {
+        double vowelsSum = countOf("a", "e", "i", "o", "u", "y");
+        double sumOfChars = size();
+        double percentage = (vowelsSum / sumOfChars) * 100;
+        return String.format("%.0f", percentage);
+    }
+
+    public String countAERatio() {
+        double countE = countOf("e");
+        double countA = countOf("a");
+        double ratio = countA / countE;
+        return String.format("%.2f", ratio);
+    }
+
+    public void showLettersPercentage() {
+        View view = new View();
+        Set<String> uniqueChars = getUniqueChars();
+        Map<String, Double> charsPercentage = new HashMap<>();
+        double sumOfChars = size();
+
+        for (String letter : uniqueChars) {
+            double letterSum = countOf(letter);
+            double percentage = (letterSum / sumOfChars) * 100;
+            charsPercentage.put(letter, percentage);
+        }
+
+        for (Map.Entry<String, Double> entry : charsPercentage.entrySet()) {
+            view.printFormattedMessage("[%s ➛ %.2f%s] ", entry.getKey(), entry.getValue(), "%");
+        }
+    }
+
+    private Set<String> getUniqueChars() {
         Set<String> uniqueChars = new HashSet<>();
         while (iterator.hasNext()) {
             String letter = iterator.next();
